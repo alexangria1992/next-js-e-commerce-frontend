db5dd324934163071bd757af57f05cff43641b3a
alexangria1992 <alex040892@gmail.com>
2023-04-12 19:15:11 +1200

    edited classnames for checkout button

--- pages/cart.js
+++ pages/cart.js
@@ -80,9 +80,7 @@ const Cart = ({ data }) => {
                 {/* BUTTON START */}
 
                 <button
-                  className="w-full py-4 rounded-full bg-black text-white text-lg font-medium 
-            transition-transform active:scale-95 mb-3 hover:opacity-75 flex items-center gap-2 justify-center 
-            bg-red-100"
+                  className="w-full py-4 rounded-full bg-black text-white text-lg font-medium transition-transform active:scale-95 mb-3 hover:opacity-75 flex items-center gap-2 justify-center"
                   onClick={handlePayment}
                 >
                   Checkout
